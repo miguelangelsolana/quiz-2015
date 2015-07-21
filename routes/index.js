d6f172e6c5a1bfccd340f16ac214fbcc0f894075
miguelangelsolana <miguelangel.solana@gmail.com>
2015-07-21 18:12:26 +0200

    Autoload de la DB

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz-2015' });
 });
+
+router.param('quizId', quizController.load);
 
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
